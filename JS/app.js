e38c4541f26fbe38b38c1ed296cffad07de05b71
Micaela <micacalvo@hotmail.es>
2022-07-29 12:13:22 -0300

    Estilos página carrito.html

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-// Carrito para compra de los servicios -- app.js se activa en carrito.html
+// Carrito para compra de los servicios
 
 // Todas las constantes a utilizar
 
@@ -8,46 +8,48 @@ const templateCards = document.getElementById("template-cards").content
 const templateFooter = document.getElementById("template-footer").content
 const templateCarrito = document.getElementById("template-carrito").content
 const footer = document.getElementById("footer")
-const vaciar = document.getElementById("vaciar-carrito")
+const fragment = document.createDocumentFragment()
 let carrito = {}
+//Como tengo que recorrer elementos, es necesario usar fragments, para evitar reflow. También lo necesito porque estoy usando templates
+//.content me sirve para recorrer los elementos
 
 // Eventos 
 
 // "DOMContentLoaded" se dispara cuando toda la página HTML esta cargada
 document.addEventListener('DOMContentLoaded', e => { 
-    fetchData()
-    if (localStorage.getItem("carrito")) {
+    fetchData(e)
+    if (localStorage.getItem("carrito")) { //Guardo en localStorage para que cuando reinicie la página, no se borre lo que guarde en el carrito
         carrito = JSON.parse(localStorage.getItem("carrito"))
         dibujoCarrito()
     }
 }); 
 
 cards.addEventListener("click", e => {
-    addCart(e)
-}) // con "e" capturo el elemento que quiero modificar, en este caso el botón comprar
+    addCart(e) // con "e" capturo el elemento que quiero modificar, en este caso el botón comprar
 
-items.addEventListener("click", e => {
-    //console.log(e)
-    btndisminuir(e)
-    /* dibujoCard()
-    dibujoCarrito()
-    dibujoFooter()
-     */ /* Swal.fire ({
+    if (e.target.classList.contains("btn-primaryA")) { 
+    Swal.fire ({
         title: '¡Bien!',
         text: 'Has agregado el servicio al carrito',
         icon: 'success',
-        position: "top-right",
+        position: "bottom",
         showConfirmButton: false,
         timer: 3000,
         width: 400,
         background: "#dadada",
         color: "#131d39"
-    }) */ 
+    })}
+}) 
+
+items.addEventListener("click", e => {
+    //console.log(e)
+   btnMenos(e)
 })
 
 // Funciones
 
 // Traigo los datos del objeto del .json
+// Con fetch accedo a los datos del .json
 
 const fetchData = async () => {
         const respuesta = await fetch("../JSON/datos.json");
@@ -60,157 +62,148 @@ const fetchData = async () => {
 
 const dibujoCard = data => {
     data.forEach(item => {
-    //console.log(item)
+    console.log(item)
         
         // Estructura de las cards
-        const cards = document.createElement("div")
-        cards.classList.add("cards") //Con classList.add agrego las clases de CSS
+        templateCards.querySelector("h5").textContent = item.nombre
+        templateCards.querySelector("p").textContent = item.precio
+        templateCards.querySelector("img").setAttribute("src", item.img)
+        templateCards.querySelector("button").dataset.id = item.id
         
-        // Body con sus elementos
-        const cardBody = document.createElement ("div");
-        cardBody.classList.add("card-body");
-
-        const imgBody = document.createElement("img");
-        imgBody.classList.add("card-img-top");
-        imgBody.setAttribute("src", item.img);
-
-        const cardTittle = document.createElement("h5");
-        cardTittle.classList.add("card-title");
-        cardTittle.textContent = item.nombre;
-
-        const cardPrecio = document.createElement("p");
-        cardPrecio.classList.add("p");
-        cardPrecio.textContent = item.precio;
-
-        const cardButton = document.createElement("button");
-        cardButton.classList.add("btn-primary")
-        cardButton.dataset.id = item.id;
-        cardButton.innerHTML = `¡Lo compro!`
- 
-    templateCards.appendChild(cards)
-    cards.appendChild(cardBody)
-    cards.appendChild(imgBody)
-    cards.appendChild(cardButton)
-    cards.appendChild(cardPrecio)
-    cards.appendChild(cardTittle)
+        const clone = templateCards.cloneNode(true) //Hago una clonación para pasar el fragment
+        fragment.appendChild(clone)
 })
-    cards.appendChild(templateCards);
+    cards.appendChild(fragment);
 }
 
 // Función para agregar al carrito
 
 const addCart = e => {
-    //console.log(e.target.classList.contains("btn-primary")) // Veo en consola si el elemento contiene la clase. Me debe tirar true o false    
-if (e.target.classList.contains("btn-primary")) {
+//console.log(e.target.classList.contains("btn-primary")) // Veo en consola si el elemento contiene la clase. Me debe tirar true o false    
+if (e.target.classList.contains("btn-primaryA")) {
     //console.log(e.target.parentElement)
-    setCart(e.target.parentElement)
+    setCart(e.target.parentElement) 
 } e.stopPropagation() // Con stopPropagation detengo cualquier otro evento que se pueda generar en "cards" que es el contenedor padre
 }
-
 
 // Función para manipular el carrito 
 // A setCart le tengo que empujar todo el objeto (nombre del servicio, id y precio)
 
-const setCart = cards => {
-    // Creo el objeto de los servicios
+const setCart = item => {
+// Creo el objeto de los servicios
     const servicio = { 
-        id: cards.querySelector(".btn-primary").dataset.id,
-        nombre: cards.querySelector("h5").textContent,
-        precio: cards.querySelector ("p").textContent,
+        id: item.querySelector("button").dataset.id,
+        nombre: item.querySelector("h5").textContent,
+        precio: item.querySelector("p").textContent,
         cantidad: 1
    }
-
+// Si el usuario quisiera aumentar la cantidad de servicios en el carrito, uso la propiedad "hasOwnProperty", que lo que hace es buscar el ID del servicio
    if (carrito.hasOwnProperty(servicio.id)) {
     servicio.cantidad = carrito[servicio.id].cantidad + 1
 }
    //console.log(servicio)
    carrito[servicio.id] = {...servicio} //Con "..." estoy haciendo una copia de los servicios, para empujarlos al carrito en su propiedad ID
-    dibujoCarrito()
+    
+dibujoCarrito()
 }
 
 // Dibujo el carrito-- Se va a ejecutar cuando agregue un servicio al carrito
 
 const dibujoCarrito = () => {
 
-    //items.innerHTML = ''  // Lo reinicio en 0 para que no se sobre escriba la información
+    items.innerHTML = ''  // Lo reinicio en 0 para que no se sobre escriba la información
 
-    // Necesito hacer un ciclo forEach, pero como no se pueden ocupar los objetos debo utilizar "Objet.values"
-    Object.values(carrito).forEach((item) => {
+    // Necesito hacer un ciclo forEach, pero como no se pueden ocupar las propiedades de un array debo utilizar "Objet.values"
+    Object.values(carrito).forEach((servicio) => {
 
         // Estructura fila del carrito
-        const name = document.createElement("row");
-        name.classList.add("name");
-        name.textContent = item.nombre
+        templateCarrito.querySelector("th").textContent = servicio.id
+        templateCarrito.querySelectorAll("td")[0].textContent = servicio.nombre
+        templateCarrito.querySelectorAll("td")[1].textContent = servicio.cantidad
+        templateCarrito.querySelector("span").textContent = servicio.precio * servicio.cantidad
+        templateCarrito.querySelector(".btn-danger").dataset.id = servicio.id
 
-        const cantidad = document.createElement("row");
-        cantidad.classList.add("cantidad");
-        cantidad.textContent = item.cantidad
-
-        const precio = document.createElement("row");
-        precio.classList.add("precio");
-        precio.textContent = item.cantidad * item.precio;
-
-        const btnvaciar = document.createElement("button");
-        btnvaciar.querySelector(".btn-danger");
-        btnvaciar.dataset.id = item.id
-
-        templateCarrito.appendChild(name)
-        templateCarrito.appendChild(cantidad)
-        templateCarrito.appendChild(precio)
-        templateCarrito.appendChild(btnvaciar);
+const clone = templateCarrito.cloneNode(true)
+fragment.appendChild(clone)
 })
-items.appendChild(templateCarrito);
+items.appendChild(fragment)
+
 dibujoFooter()
-localStorage.setItem("carrito", JSON.stringify("carrito"))
+
+localStorage.setItem("carrito", JSON.stringify(carrito))
+
 } 
 
 // Cuando se agregue un servicio al carrito, se ejecuta el footer. El footer cuando esta vacio dice "Comience a comprar", if lo uso para que cambie cuando el usuario agregue los servicios al carrito o vacie el carrito
 
 const dibujoFooter = () => {
-    footer.innerHTML = '' 
+    footer.innerHTML = ''
+
     if (Object.keys(carrito).length === 0) { // Keys y length cuentan si hay elementos en el carrito 
         footer.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío - ¡Comience a comprar!</th>`
+        <th scope="row" colspan="5">Carrito vacío - ¡Comience a comprar!</th>
+        `
         return
     }
 
-const sumaCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0);   //Vuelvo a usar el "Object.values" para acceder al carrito y "cantidad" para que en cada iteración me vaya sumando lo anterior
-const sumaTotal = document.createElement("div");
-sumaTotal.classList.add("sumaCantidad");
-sumaTotal.textContent = sumaCantidad
+const sumaCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad, 0); //Vuelvo a usar el "Object.values" para acceder al carrito y "cantidad" para que en cada iteración me vaya sumando lo anterior
 
 const precioTotal = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio, 0);
-const precioFinal = document.createElement("div");
-precioFinal.classList.add("precioTotal");
-precioFinal.textContent = precioTotal
 
-templateFooter.appendChild(sumaTotal)
-templateFooter.appendChild(precioFinal)
-footer.appendChild(templateFooter)
+templateFooter.querySelectorAll("td")[0].textContent = sumaCantidad
+templateFooter.querySelector("span").textContent = precioTotal
 
-const vaciarCart = document.querySelector("#vaciar-carrito")
-vaciarCart.addEventListener("click", () => {
+const clone = templateFooter.cloneNode(true)
+fragment.appendChild(clone)
+footer.appendChild(fragment)
+
+const vaciarCart = document.getElementById("vaciar-carrito")
+vaciarCart.addEventListener("click", (e) => {
     carrito = {}
     dibujoCarrito()
-})
-}
 
-const btndisminuir = e => {
-    if(servicio.cantidad === 0) {
-        delete carrito [e.target.dataset.id]
-    }
-dibujoCarrito();
-}
-   /*  dibujoCarrito()
-  Swal.fire ({
+if (e.target.classList.contains("btn-danger")){
+    Swal.fire ({
         title: '¡NO!',
-        text: "Se vaciara el carrito",
+        text: "Se eliminara el servicio del carrito",
         icon: 'warning',
         position: "bottom",
-        showCancelButton: true,
+        showCancelButton: false,
         showConfirmButton: false,
         timer: 3000,
         width: 400,
         background: "#dadada",
         color: "rgb(0, 0, 0)",
-})  */
+})
+}
+})
+}
+
+const btnMenos = e => { //Función para disminuir un solo objeto del carrito 
+   // console.log(e.target.classList.contains("btn-danger"))
+    if(e.target.classList.contains("btn-danger")) {
+        const servicio = carrito[e.target.dataset.id]
+        servicio.cantidad--
+        if(servicio.cantidad === 0) {
+            delete carrito[e.target.dataset.id] //Con delete cuando llega a 0 borra la línea del carrito 
+        } else {
+            carrito[e.target.dataset.id] = {...servicio}
+        }
+        dibujoCarrito() 
+       
+if (e.target.classList.contains("btn-danger")){
+    Swal.fire ({
+        title: '¡NO!',
+        text: "Se eliminara el servicio del carrito",
+        icon: 'warning',
+        position: "bottom",
+        showCancelButton: false,
+        showConfirmButton: false,
+        timer: 3000,
+        width: 400,
+        background: "#dadada",
+        color: "rgb(0, 0, 0)",
+    })
+}
+} e.stopPropagation()
+} 
